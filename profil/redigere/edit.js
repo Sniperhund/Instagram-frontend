d1550f8ed465192a52aad91b2391b3a63860d2e3
Lucas Täkker <50625016+Sniperhund@users.noreply.github.com>
2024-05-15 13:33:34 +0200

    Yes 3

---
+++ profil/redigere/edit.js
@@ -0,0 +1,7 @@
+var modalContainer = document.getElementById("modal-container")
+const modal = document.getElementById("modal")
+
+function toggleModal() {
+    modalContainer.classList.toggle("open")
+    modal.classList.toggle("animation")
+}
