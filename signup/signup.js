acb852cb0ac3c13c9fe7ed27b8889fe6d1d0bb7f
Atlas <68791999+Atlas7005@users.noreply.github.com>
2024-05-15 11:56:59 +0200

    Add signup

--- signup/signup.js
+++ signup/signup.js
@@ -1,4 +1,5 @@
 let inputs = document.getElementsByTagName("label")
+let form = document.querySelector("form")
 
 document.addEventListener("input", () => {
     for (let i = 0; i < inputs.length; i++) {
@@ -11,3 +12,32 @@ document.addEventListener("input", () => {
         }
     }
 })
+
+form.onsubmit = (e) => {
+    e.preventDefault();
+    let data = {}
+
+    for (let i = 0; i < inputs.length; i++) {
+        const input = inputs[i].children[1]
+        data[input.name] = input.value
+    }
+
+    fetch("https://instagrom.masrmedia.dk/api/auth/signup", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    })
+    .then(res => res.json())
+    .then(data => {
+        if (data.accessToken) {
+            setCookie("accessToken", data.accessToken, 1)
+            window.location.assign("/")
+        }
+    })
+    .catch(err => {
+        console.error(err)
+        alert("An error occurred")
+    })
+}
