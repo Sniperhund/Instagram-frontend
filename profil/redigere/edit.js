8fb454a0be6d53b49b8af2901e6adaba3931de5b
Atlas7005 <atlasdevelopmentbusiness@gmail.com>
2024-05-16 10:09:21 +0200

    added pfp upload & deletion

--- profil/redigere/edit.js
+++ profil/redigere/edit.js
@@ -5,3 +5,48 @@ function toggleModal() {
     modalContainer.classList.toggle("open")
     modal.classList.toggle("animation")
 }
+
+function upload(e) {
+    e.preventDefault()
+    const fileInput = document.createElement("input")
+    fileInput.type = "file"
+    fileInput.accept = "image/*"
+    fileInput.onchange = (e) => {
+        const file = e.target.files[0]
+        const formData = new FormData()
+        formData.append("image", file)
+    
+        fetch("https://instagrom.masrmedia.dk/api/user", {
+            method: "PATCH",
+            headers: {
+                Authorization: getCookie("accessToken"),
+            },
+            body: formData,
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                updateProfilePicture()
+                toggleModal()
+            })
+    }
+
+    fileInput.click()
+}
+
+function remove(e) {
+    e.preventDefault()
+
+    fetch("https://instagrom.masrmedia.dk/api/user/"+window.user.id+"/avatar", {
+        method: "DELETE",
+        headers: {
+            Authorization: getCookie("accessToken"),
+        },
+    })
+        .then((res) => res.json())
+        .then((data) => {
+            updateProfilePicture()
+            toggleModal()
+        })
+}
+
+updateProfilePicture()
